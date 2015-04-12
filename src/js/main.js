4d717a1afb343c857187595e5b6c0b6c8f6f6ec8
Joanne <joannemah.j.w@gmail.com>
2015-04-12 15:02:25 +0800

    Animated hide/show black menu bar function

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-    $(".pre-emails").slimscroll({
-        height: 'auto'
-    });
+	$(".pre-emails").slimscroll({
+		height: 'auto'
+	});
+});
+
+// menu
+$(".close-menu").click( function() {
+	// var toggleWidth = $(".menu").width() == 50 ? "15%" : "50px";
+	// $(".menu").animate({ width: toggleWidth });
+	
+	if ($(".menu").width() == 50) {
+		$(".menu").animate({ width: "15%" });
+		$(".min-max .row").css("padding", "0 20px");
+		$(".category-header").css("display", "block");
+		$(".categories a").css("display", "inline");
+	} else {
+		$(".menu").animate({ width: "50px" });
+		$(".min-max .row").css("padding", "0 15px");
+		$(".category-header").css("display", "none");
+		$(".categories a").css("display", "none");
+	}
+
 });
