ba0cea8bd88c9018dae753b2c8b23c4cd5292db6
Sue Zheng Hao <szhlibrary@gmail.com>
2015-04-13 12:54:51 +0800

    so apparently the IVLE API gives you the raw HTML. I guess we should handle it now in the rare chance that we decide to actually have a backend :P

--- src/js/dummyAnnouncements.js
+++ src/js/dummyAnnouncements.js
@@ -18,27 +18,8 @@ var announcements = [
 	moduleCode: "CS3240",
 	author: "Bimlesh Wadhwa",
 	title: "Poster Printing for STePS",
-	content: "FYI - from STePS team\n\nPOSTER PRINTING:-  The project group leaders can approach SoC technical " +
-	"services with your student card to print one free copy of your poster from 14th Apr. Please go with high " +
-	"resolution version (at least 300 dpi) of your poster in PDF format. 12pm-2pm is lunchtime for SoC technical " +
-	"services, please avoid this slot.The following dates are assigned for each module. We recommend you to print your " +
-	"posters on or before the date assigned for your module.\n\n	CS 3240: 15 Apr\n\nEach poster should carry the " +
-	"project ID (top left cornet, big font size), title, sponsors logo (bottom). More details about this is available " +
-	"at STePS website - FAQ section. [Use only those logo images provided in the FAQ section. These are HIGH-RESOLUTION " +
-	"logos.].\n\nYour project id will be in the form - \"####-##\"available at : " +
-	"http://steps.comp.nus.edu.sg/steps6/abstracts.html.\n\n	Poster Guidelines\n\nPosters size is A1 (841 x 594 mm)." +
-	"\nIt should consist of approx 25% text, 45% graphics and 30% empty space (do not cram the poster full with " +
-	"information).\nThe poster should be printed on one sheet.\n	The poster material should be organised into " +
-	"sections presented in a logical sequence which is easy for the viewer to follow (usually top-bottom, left-right," +
-	" with the Introduction at top left and Conclusions at bottom right).\nTry to keep text to a minimum; use short " +
-	"sentences (or sentence fragments) and paragraphs to convey your message.\n	The text font size should be legible " +
-	"from at least 1.5m (18 pt), with larger (at least 32pt) section headings; bold text is easier to read from a " +
-	"distance.\n	Don’t use too many different fonts as this can distract the viewer’s attention, as can too many " +
-	"bright clashing colours.\n	Graphics (and photos), large enough to be clearly discernible from 1.5m, should be " +
-	"used wherever possible. Label graphs directly (no legends, keys) and use colour, rather than patterns, to " +
-	"distinguish different data sets and areas of interest. Avoid combining red and green for those viewers who may be " +
-	"red/green colour blind. Make sure you use an adequate font size for graphics labels to be legible from 1.5m.",
-	files: "", 
+	content: "<div>	&nbsp;</div><div>FYI - from STePS team&nbsp;</div><div>&nbsp;</div><div>POSTER PRINTING:- &nbsp;The project group leaders can approach SoC technical services with your student card to print one free copy of your poster from 14th Apr. Please go with high resolution version (at least 300 dpi) of your poster in PDF format. 12pm-2pm is lunchtime for SoC technical services, please avoid this slot.The following dates are assigned for each module. We recommend you to print your posters on or before the date assigned for your module. &nbsp;</div><div>&nbsp;</div><div>&nbsp;CS 3240: 15 Apr</div><div>&nbsp;</div><div>Each poster should carry the project ID (top left cornet, big font size), title, sponsors logo (bottom). More details about this is available at STePS website - FAQ section. [Use only those logo images provided in the FAQ section. These are HIGH-RESOLUTION logos.].&nbsp;</div><div>&nbsp;</div><div>Your project id will be in the form - &quot;####-##&quot;available at : http://steps.comp.nus.edu.sg/steps6/abstracts.html.</div><div>&nbsp;</div><div>Poster Guidelines</div><div>&nbsp;</div><div>Posters size is A1 (841 x 594 mm).&nbsp;</div><div>It should consist of approx 25% text, 45% graphics and 30% empty space (do not cram the poster full with information).&nbsp;</div><div>The poster should be printed on one sheet.</div><div>The poster material should be organised into sections presented in a logical sequence which is easy for the viewer to follow (usually top-bottom, left-right, with the Introduction at top left and Conclusions at bottom right).</div><div>Try to keep text to a minimum; use short sentences (or sentence fragments) and paragraphs to convey your message.&nbsp;</div><div>The text font size should be legible from at least 1.5m (18 pt), with larger (at least 32pt) section headings; bold text is easier to read from a distance.</div><div>Don&rsquo;t use too many different fonts as this can distract the viewer&rsquo;s attention, as can too many bright clashing colours.</div><div>Graphics (and photos), large enough to be clearly discernible from 1.5m, should be used wherever possible. Label graphs directly (no legends, keys) and use colour, rather than patterns, to distinguish different data sets and areas of interest. Avoid combining red and green for those viewers who may be red/green colour blind. Make sure you use an adequate font size for graphics labels to be legible from 1.5m.<br>&nbsp;</div>",
+	files: "",
 	time: new Date("April 12, 2015 13:45:00"),
 	favourited: 1
     // reminder added?
