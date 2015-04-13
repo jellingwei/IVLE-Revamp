f6acfca96b9ffa47a67edceb07e741da951f396a
Sue Zheng Hao <szhlibrary@gmail.com>
2015-04-13 13:00:50 +0800

    Add a read boolean variable

--- src/js/dummyAnnouncements.js
+++ src/js/dummyAnnouncements.js
@@ -10,6 +10,7 @@ var announcements = [
 	" post the solutions for HW12 after the dure date. Feel free to email me of you have any quesitons about them.\n--LB",
 	files: "",
 	time: new Date("April 13, 2015 4:33:00"),
+	read: false,
 	favourited: 0
     // reminder added?
     // tags?
@@ -21,6 +22,7 @@ var announcements = [
 	content: "<div>	&nbsp;</div><div>FYI - from STePS team&nbsp;</div><div>&nbsp;</div><div>POSTER PRINTING:- &nbsp;The project group leaders can approach SoC technical services with your student card to print one free copy of your poster from 14th Apr. Please go with high resolution version (at least 300 dpi) of your poster in PDF format. 12pm-2pm is lunchtime for SoC technical services, please avoid this slot.The following dates are assigned for each module. We recommend you to print your posters on or before the date assigned for your module. &nbsp;</div><div>&nbsp;</div><div>&nbsp;CS 3240: 15 Apr</div><div>&nbsp;</div><div>Each poster should carry the project ID (top left cornet, big font size), title, sponsors logo (bottom). More details about this is available at STePS website - FAQ section. [Use only those logo images provided in the FAQ section. These are HIGH-RESOLUTION logos.].&nbsp;</div><div>&nbsp;</div><div>Your project id will be in the form - &quot;####-##&quot;available at : http://steps.comp.nus.edu.sg/steps6/abstracts.html.</div><div>&nbsp;</div><div>Poster Guidelines</div><div>&nbsp;</div><div>Posters size is A1 (841 x 594 mm).&nbsp;</div><div>It should consist of approx 25% text, 45% graphics and 30% empty space (do not cram the poster full with information).&nbsp;</div><div>The poster should be printed on one sheet.</div><div>The poster material should be organised into sections presented in a logical sequence which is easy for the viewer to follow (usually top-bottom, left-right, with the Introduction at top left and Conclusions at bottom right).</div><div>Try to keep text to a minimum; use short sentences (or sentence fragments) and paragraphs to convey your message.&nbsp;</div><div>The text font size should be legible from at least 1.5m (18 pt), with larger (at least 32pt) section headings; bold text is easier to read from a distance.</div><div>Don&rsquo;t use too many different fonts as this can distract the viewer&rsquo;s attention, as can too many bright clashing colours.</div><div>Graphics (and photos), large enough to be clearly discernible from 1.5m, should be used wherever possible. Label graphs directly (no legends, keys) and use colour, rather than patterns, to distinguish different data sets and areas of interest. Avoid combining red and green for those viewers who may be red/green colour blind. Make sure you use an adequate font size for graphics labels to be legible from 1.5m.<br>&nbsp;</div>",
 	files: "",
 	time: new Date("April 12, 2015 13:45:00"),
+	read: true,
 	favourited: 1
     // reminder added?
     // tags?
@@ -32,6 +34,7 @@ var announcements = [
 	content: "asd",
 	files: "", 
 	time: new Date("April 4, 2015 12:13:00"),
+	read: false,
 	favourited: 0
     // reminder added?
     // tags?
@@ -43,6 +46,7 @@ var announcements = [
 	content: "I gave Rusty a bone and he gave me a smile! I was so overwhelmed with joy I just had to take out my Canon 60D and snap the moment. I’m thinking about getting it framed, know of any places that do cheap frames?Let me know how things are with you!",
 	files: "", 
 	time: new Date("April 5, 2015 12:13:00"),
+	read: false,
 	favourited: 0
     // reminder added?
     // tags?
