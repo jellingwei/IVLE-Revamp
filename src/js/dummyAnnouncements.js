9531a13fd8c27d424af8c85009501742e3e0886c
Sue Zheng Hao <szhlibrary@gmail.com>
2015-04-13 11:34:28 +0800

    Some dummy data

--- src/js/dummyAnnouncements.js
+++ src/js/dummyAnnouncements.js
@@ -2,27 +2,51 @@ console.log("dummy announcements loaded");
 // dummy data for announcements
 var announcements = [
 { 
-	moduleCode: "CS3217",
-	title: "New Files in Workbin", 
-	content: "<p>cs2106lect11.pdf was uploaded by by Tan Keng Yan, Colin in Colin's Lecture Notes</p>", 
-	files: "cs2106lect11.pdf", 
-	time: new Date("April 5, 2015 12:13:00"),
+	moduleCode: "CS2106",
+	author: "Lubomir Bic",
+	title: "Last lecture on Fri April 17",
+	content: "<p>The original plan was to discuss the last homework (HW12) during the last lecture slot (since there are" +
+	" no more tutorials next week).\nHowever, Prof. Tan has a different lacture planned for that date.\nHence I will" +
+	" post the solutions for HW12 after the dure date. Feel free to email me of you have any quesitons about them.\n--LB</p>",
+	files: "",
+	time: new Date("April 13, 2015 4:33:00"),
 	favourited: 0
     // reminder added?
     // tags?
 },
 { 
-	moduleCode: "LSM1303",
-	title: "Symposium I updated slides, Symposium II admin", 
-	content: "<p>The first part of this announcement applies to students who presented in symposium I ...</p>", 
+	moduleCode: "CS3240",
+	author: "Bimlesh Wadhwa",
+	title: "Poster Printing for STePS",
+	content: "<p>FYI - from STePS team\n\nPOSTER PRINTING:-  The project group leaders can approach SoC technical " +
+	"services with your student card to print one free copy of your poster from 14th Apr. Please go with high " +
+	"resolution version (at least 300 dpi) of your poster in PDF format. 12pm-2pm is lunchtime for SoC technical " +
+	"services, please avoid this slot.The following dates are assigned for each module. We recommend you to print your " +
+	"posters on or before the date assigned for your module.\n\n	CS 3240: 15 Apr\n\nEach poster should carry the " +
+	"project ID (top left cornet, big font size), title, sponsors logo (bottom). More details about this is available " +
+	"at STePS website - FAQ section. [Use only those logo images provided in the FAQ section. These are HIGH-RESOLUTION " +
+	"logos.].\n\nYour project id will be in the form - \"####-##\"available at : " +
+	"http://steps.comp.nus.edu.sg/steps6/abstracts.html.\n\n	Poster Guidelines\n\nPosters size is A1 (841 x 594 mm)." +
+	"\nIt should consist of approx 25% text, 45% graphics and 30% empty space (do not cram the poster full with " +
+	"information).\nThe poster should be printed on one sheet.\n	The poster material should be organised into " +
+	"sections presented in a logical sequence which is easy for the viewer to follow (usually top-bottom, left-right," +
+	" with the Introduction at top left and Conclusions at bottom right).\nTry to keep text to a minimum; use short " +
+	"sentences (or sentence fragments) and paragraphs to convey your message.\n	The text font size should be legible " +
+	"from at least 1.5m (18 pt), with larger (at least 32pt) section headings; bold text is easier to read from a " +
+	"distance.\n	Don’t use too many different fonts as this can distract the viewer’s attention, as can too many " +
+	"bright clashing colours.\n	Graphics (and photos), large enough to be clearly discernible from 1.5m, should be " +
+	"used wherever possible. Label graphs directly (no legends, keys) and use colour, rather than patterns, to " +
+	"distinguish different data sets and areas of interest. Avoid combining red and green for those viewers who may be " +
+	"red/green colour blind. Make sure you use an adequate font size for graphics labels to be legible from 1.5m.</p>",
 	files: "", 
-	time: new Date("April 4, 2015 12:13:00"),
+	time: new Date("April 12, 2015 13:45:00"),
 	favourited: 1
     // reminder added?
     // tags?
 },
 { 
 	moduleCode: "CS2106",
+	author: "",
 	title: "Final Project Assessment Meeting Times", 
 	content: "<p>Hello everyone, </p> <p>The times for the final project assessment meetings have ben fixed.  For ...</p>", 
 	files: "", 
@@ -33,6 +57,7 @@ var announcements = [
 },
 { 
 	moduleCode: "",
+	author: "",
 	title: "I taught my dog how to smile!", 
 	content: "<p>I gave Rusty a bone and he gave me a smile!</p><p>I was so overwhelmed with joy I just had to take out my Canon 60D and snap the moment. I’m thinking about getting it framed, know of any places that do cheap frames?</p><p>Let me know how things are with you!</p>", 
 	files: "", 
