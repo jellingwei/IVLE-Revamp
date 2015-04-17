01540e9e251c8e2c3177a88606b1a93ca493d2b5
Joanne <joannemah.j.w@gmail.com>
2015-04-17 22:12:38 +0800

    Added additional form inputs for reminders

--- src/js/popover.js
+++ src/js/popover.js
@@ -1,21 +1,76 @@
-// reminders
-var date = new Date();
+moment.locale('en', {
+    calendar : {
+        lastDay : '[Yesterday]',
+        sameDay : '[Today]',
+        nextDay : '[Tomorrow]',
+        lastWeek : '[last] dddd [at] LT',
+        nextWeek : '[Next Week]',
+        sameElse : 'L'
+    }
+});
 
-// clock
 $('.clockpicker').clockpicker({
 	placement: 'bottom',
     align: 'left',
-    autoclose: true,
-    'default': 'now'
+    donetext: 'Done',
+    'default': 'now',
+    autoclose: true
 
 });
 
-var time = (date.getHours() + 1) + ':' + date.getMinutes();
-$('.reminders.time-input').val(time)
+
+$('.reminder-8am').click(function() {
+	$('.reminders.time-input').val('08:00');
+});
+
+$('.reminder-12pm').click(function() {
+	$('.reminders.time-input').val('12:00');
+});
+
+$('.reminder-6pm').click(function() {
+	$('.reminders.time-input').val('18:00');
+});
+
+$('.reminder-10pm').click(function() {
+	$('.reminders.time-input').val('22:00');
+});
+
+$('.reminders.time-input').val(moment().format('hh:mm'));
 
 // calendar
-$('#datetimepicker1').datetimepicker({
+$('#datetimepicker').datetimepicker({
 	 format: 'DD/MM/YYYY'
+})
+
+$('.reminder-today').click(function() {	
+	$('.reminders.date-input').val(moment().format('DD/MM/YYYY'));
+
 });
-var date = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
-$('.reminders.date-input').val(date);
+
+var tomorrow = moment().add(1, 'days');
+$('.reminder-tomorrow').click(function() {	
+	$('.reminders.date-input').val(tomorrow.format('DD/MM/YYYY'));
+
+});
+
+var nextWeek = moment().add(1, 'weeks');
+$('.reminder-nextWeek').click(function() {
+	$('.reminders.date-input').val(nextWeek.format('DD/MM/YYYY'));
+});
+
+var nextMonth = moment().add(1, 'months');
+$('.reminder-nextMonth').click(function() {
+	$('.reminders.date-input').val(nextMonth.format('DD/MM/YYYY'));
+});
+
+$('.reminders.date-input').val(moment().format('DD/MM/YYYY'));
+
+// save changes
+$('.reminder-save').click(function() {
+	console.log("saved:");
+	console.log($('.reminders.date-input').val());
+	console.log($('.reminders.time-input').val());
+	$('.modal-body').html("");
+	$('.modal-body').append("<h4>Reminder added</h4>")
+	$('.modal-footer').html("");
+})
